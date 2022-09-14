ceff6f988bf1c96565b80141cb11689a4a341609
thib <umut@Umuts-MacBook-Pro.local>
2022-09-14 16:54:39 +1000

    new components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,20 +1,18 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
 
-const Expenses = (props) => { 
-
-
+const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
-   const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
@@ -25,35 +23,10 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
 };
-// return (
-//   <div>
-//     <Card className="expenses">
-//       <ExpensesFilter
-//         selected={filteredYear}
-//         onChangeFilter={filterChangeHandler}
-//       />
-//       {props.items.map((expense) => (
-//         <ExpenseItem
-//           key={expense.id}
-//           title={expense.title}
-//           amount={expense.amount}
-//           date={expense.date}
-//         />
-//       ))}
-//     </Card>
-//   </div>
-// );
-// };
+
 export default Expenses;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,21 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isToggle, setIsToggle] = useState(false)
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsToggle(false)
   };
 
+const toggleSwitchOn = () => {
+  setIsToggle(true);
+}
+const toggleSwitchOff = () => {
+  setIsToggle(false)
+}
   return (
+    <div>
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    {!isToggle && <button onClick={toggleSwitchOn}>Add a New Expense</button>}
+    
+    {isToggle && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={toggleSwitchOff}/>}
+      
     </div>
+        </div>
   );
 };
 
